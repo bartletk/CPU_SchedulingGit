9bc44a9e19aaf319b4653c9134b94bb23758acac
bartletk <bartletk@users.noreply.github.com>
2018-03-24 15:49:07 -0500

    Can't seem to get R_r working right.

--- src/R_r.java
+++ src/R_r.java
@@ -2,8 +2,36 @@
  * R_r Class
  * "Round-Robin"
  */
-public class R_r extends Process 
+public class R_r extends Process implements Comparable<R_r> 
 {
 
+	private int tat, wt;
+	
+	/*
+	 * Overrides Process compareTo()
+	 * Returns whichever object has the smaller burst time.
+	 * @return integer the shortest burst time.
+	 */
+	@Override
+	public int compareTo(R_r compareProc) 
+    {
+        int compareBurst=(compareProc).getBurst();
+        		/* For Ascending order*/
+        return this.burst-compareBurst;
+		        //For Descending order do like this;
+		        //return comparePriority-this.priority;
+    }
+	
+	public int getTat()
+	{
+		return tat;
+	}
+	
+	
+	public void setTat(int tat)
+	{
+		this.tat = tat;
+	}
+	
 
 }
